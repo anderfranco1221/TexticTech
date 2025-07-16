a3e705a748307825632094e0dcb88a3c2c5aeeaf
anderfranco1221 <anderfranco1221@gmail.com>
2025-07-15 21:05:09 -0500

    style: :lipstick: Correcion de los estilos del login

--- apps/frontend/src/views/Loggin/LogginView.vue
+++ apps/frontend/src/views/Loggin/LogginView.vue
@@ -4,22 +4,22 @@
 <template>
     <div class="background-loggin">
         <div
-            class="card-loggin" @keydown.enter="handleSubmit">
-            <img src="../../assets/image/logo.png" />
+            class="card-loggin bg-primary" @keydown.enter="handleSubmit">
+            <img src="../../assets/image/logo-white.png" class="h-50 w-auto"/>
 
-            <div class="mt-6 w-80 pt-3.5">
+            <div class="mt-6 w-80">
                 <label for="username" class="text-md">
                     Usuario
                 </label>
-                <input type="text" placeholder="Email" id="username" v-model="email"/>
+                <input class="bg-muted" type="text" placeholder="Email" id="username" v-model="email"/>
             </div>
             <div class="mt-6 w-80 pt-3.5">
                 <label for="password" class="text-md">
                     Contraseña
                 </label>
-                <input type="password" placeholder="Contraseña" id="password" v-model="password"/>
+                <input class="bg-muted" type="password" placeholder="Contraseña" id="password" v-model="password"/>
             </div>
-            <div class="mt-6 pt-3.5">
+            <div class="mt-6 pt-8">
                 <button @click="handleSubmit">
                     Ingresar
                 </button>
